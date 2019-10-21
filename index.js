f588b491198836c02e7183ab8685e041be9de68e
nerdaniel <56851815+nerdaniel@users.noreply.github.com>
2019-10-22 01:57:26 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () =>{
+    console.log ('This bot is online!');
+
+client.login(process.env.BOT_TOKEN);
