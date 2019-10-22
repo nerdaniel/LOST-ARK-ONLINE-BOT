9b7af632e7a9fddc1f8d176f62146811f7e720f5
nerdaniel <56851815+nerdaniel@users.noreply.github.com>
2019-10-22 02:28:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
-client.on('ready', () =>{
+const token = 'NjM1OTIxMjA0OTU5NjQxNjIx.Xa5L0g.E4Obw8m3-LhS4Ub_MC9svv6neRs' ;
+
+const PREFIX = '-';
+
+bot.on('ready', () =>{
     console.log ('This bot is online!');
-client.login(process.env.BOT_TOKEN);
+})
+
+bot.on('message', message=>{
+     
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'lostarkbot' :
+            message.channel.sendMessage('Welcome, this is commands list: -server, -installazione, work in progress.')
+            break;
+            case 'server':
+                message.channel.sendMessage('Sirion [Сирион]')
+            break;
+            case 'installazione' :
+                message.channel.sendMessage('https://www.youtube.com/watch?v=alpGI-wqKmk&t=6s')
+            break;
+            case 'pulisci':
+                if(!message.member.roles.find( r => r.name === "gattoschifomadò" )) return message.channel.send('non hai i permessi!')
+                if(!args[1]) return message.reply ('errore inserisci il numero di righe righe')
+                message.channel.bulkDelete(args[1]);
+            break;
+    }
+})
+
+bot.login(token);
